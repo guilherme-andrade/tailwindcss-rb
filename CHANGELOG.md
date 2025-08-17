13836dc454f7faeca5cf9a1fa9a22a83bb401f3e
Guilherme  Andrade <gandrade@remotelock.com>
2025-08-18 00:45:53 +0200

    Release v0.2.2: Fix Channel vite_asset_path error
    ### Fixed
    - Fixed undefined method vite_asset_path error in Compiler::Channel
    - Removed unnecessary AssetHelper dependency from Channel class
    - Channel now uses Tailwindcss.tailwind_css_file_path directly
    
    This fixes the NoMethodError when ActionCable tries to broadcast CSS changes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.2] - 2024-08-17
+
+### Fixed
+- Fixed undefined method `vite_asset_path` error in Compiler::Channel
+- Removed unnecessary AssetHelper dependency from Channel class
+- Channel now uses `Tailwindcss.tailwind_css_file_path` directly
+
 ## [0.2.1] - 2024-08-17
 
 ### Fixed
--- lib/tailwindcss/compiler/channel.rb
+++ lib/tailwindcss/compiler/channel.rb
@@ -1,16 +1,13 @@
-require "tailwindcss/asset_helper"
-
 module Tailwindcss
   module Compiler
     class Channel < ActionCable::Channel::Base
-      extend AssetHelper
 
       def subscribed
         stream_from "compiler_channel"
       end
 
       def self.broadcast_css_changed
-        css_path = vite_asset_path("style.css")
+        css_path = Tailwindcss.tailwind_css_file_path
         ActionCable.server.broadcast("compiler_channel", {css_path:})
       end
     end
